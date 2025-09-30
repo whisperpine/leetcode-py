1bebfc58517ae45b50128151fd8e68b8fb092a9f
Yusong <yusonglai64@gmail.com>
2025-10-01 07:12:05 +0800

    chore: setup dev environment by nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A Nix-flake-based Python development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import inputs.nixpkgs { inherit system; }; }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              python313
+              uv # python package and project manager
+              just # just a command runner
+            ];
+            shellHook = ''
+              uv sync
+              source .venv/bin/activate
+            '';
+          };
+        }
+      );
+    };
+}
