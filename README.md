4a546489fde1368186f7fdb50da2e6f3f332c587
Yusong <yusonglai64@gmail.com>
2025-10-01 16:53:21 +0800

    feat(array-hash): add 128_longest_consecutive_sequence.py

--- README.md
+++ README.md
@@ -36,3 +36,4 @@ pytest path/to/test_file.py::test_function_name
 | 49. Group Anagrams | Medium | [49_group_anagrams.py](./tests/49_group_anagrams.py) |
 | 347. Top K Frequent Elements | Medium | [347_top_k_frequent_elements.py](./tests/347_top_k_frequent_elements.py) |
 | 238. Product of Array Except Self | Medium | [238_product_of_array_except_self.py](./tests/238_product_of_array_except_self.py) |
+| 128. Longest Consecutive Sequence | Medium | [128_longest_consecutive_sequence.py](./tests/128_longest_consecutive_sequence.py) |
---
+++ tests/128_longest_consecutive_sequence.py
@@ -0,0 +1,55 @@
+# 128. Longest Consecutive Sequence
+# https://leetcode.com/problems/longest-consecutive-sequence/description/
+# Topics: Arrays & Hashing.
+# Difficulty: Medium.
+
+
+def test_case_1() -> None:
+    assert Solution().longestConsecutive([100, 4, 200, 1, 3, 2]) == 4
+
+
+def test_case_2() -> None:
+    assert Solution().longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]) == 9
+
+
+def test_case_3() -> None:
+    assert Solution().longestConsecutive([1, 0, 1, 2]) == 3
+
+
+# ---------------------------------
+# copy to leetcode starts from here
+# ---------------------------------
+
+
+def get_left_length(n: int, s: set[int]) -> int:
+    result: int = 0
+    if n in s:
+        s.remove(n)
+        result += 1 + get_left_length(n - 1, s)
+    return result
+
+
+def get_right_length(n: int, s: set[int]) -> int:
+    result: int = 0
+    if n in s:
+        s.remove(n)
+        result += 1 + get_right_length(n + 1, s)
+    return result
+
+
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        result: int = 0
+        s: set[int] = set(nums)
+
+        left_len: int = 0
+        right_len: int = 0
+
+        for n in nums:
+            if n in s:
+                s.remove(n)
+                left_len = get_left_length(n - 1, s)
+                right_len = get_right_length(n + 1, s)
+                result = max(result, 1 + left_len + right_len)
+
+        return result
