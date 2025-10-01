38edfcd0b7bbc7fb78cf05b6de57e23243458c32
Yusong <yusonglai64@gmail.com>
2025-10-01 15:29:05 +0800

    feat(array-hash): add 347_top_k_frequent_elements.py

--- README.md
+++ README.md
@@ -34,3 +34,4 @@ pytest path/to/test_file.py::test_function_name
 | 217. Contains Duplicate | Easy | [217_contains_duplicate.py](./tests/217_contains_duplicate.py) |
 | 242. Valid Anagram | Easy | [242_valid_anagram.py](./tests/242_valid_anagram.py) |
 | 49. Group Anagrams | Medium | [49_group_anagrams.py](./tests/49_group_anagrams.py) |
+| 347. Top K Frequent Elements | Medium | [347_top_k_frequent_elements.py](./tests/347_top_k_frequent_elements.py) |
---
+++ tests/347_top_k_frequent_elements.py
@@ -0,0 +1,35 @@
+# 347. Top K Frequent Elements
+# https://leetcode.com/problems/top-k-frequent-elements/description/
+# Topics: Arrays & Hashing.
+# Difficulty: Medium.
+
+
+def test_case_1() -> None:
+    nums: list[int] = [1, 1, 1, 2, 2, 3]
+    output: list[int] = [1, 2]
+    assert set(Solution().topKFrequent(nums, 2)) == set(output)
+
+
+def test_case_2() -> None:
+    nums: list[int] = [1]
+    output: list[int] = [1]
+    assert set(Solution().topKFrequent(nums, 1)) == set(output)
+
+
+def test_case_3() -> None:
+    nums: list[int] = [1, 2, 1, 2, 1, 2, 3, 1, 3, 2]
+    output: list[int] = [1, 2]
+    assert set(Solution().topKFrequent(nums, 2)) == set(output)
+
+
+# ---------------------------------
+# copy to leetcode starts from here
+# ---------------------------------
+
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        c: Counter[int] = Counter(nums)  # num, counts
+        return [num[0] for num in c.most_common(k)]
